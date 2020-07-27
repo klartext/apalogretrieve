a5c03ad1459d5e727c47fadd207c430c7f0e6977
Oliver Bandel <oliver@first.in-berlin.de>
2020-07-27 17:33:23 +0200

    Ergebnis nun doch wieder lont int. Untermodul Strptime.

--- external.ml
+++ external.ml
@@ -1,5 +1,9 @@
 
 
-external unixtime_of_string: string -> float = "unixtime_of_string"
+module Strptime =
+  struct
+    external unixtime_of_string: string -> string -> int = "unixtime_of_string"
+    (* raises exception Failure on parsing error *)
+  end
 
 
--- unixbindings.c
+++ unixbindings.c
@@ -1,34 +1,52 @@
+#define CAML_NAME_SPACE
 #include <caml/mlvalues.h>
 #include <caml/compatibility.h>
 #include <caml/alloc.h>
 #include <caml/memory.h>
 #include <caml/fail.h>
 
+//#define _XOPEN_SOURCE
+#define __USE_XOPEN
+#include <time.h>
+#include <string.h>
 
 /* ============================================= */
 /*                                               */
 /* ============================================= */
-CAMLprim value unixtime_of_string( value name_arg )
+value unixtime_of_string( value datetime_string, value format_string )
 {
-  CAMLparam1( name_arg );
+
+  CAMLparam2( datetime_string, format_string );
   CAMLlocal1( result );
-  result = caml_alloc (1, 0);
+  char     *rest;
+  struct   tm timestruct;
+  long int time;
 
-  double time = 3.1415926;
+  char* timestring = String_val(datetime_string);
+  memset(&timestruct, 0, sizeof(struct tm));
 
-  char* name = String_val(name_arg);
+  printf("Erster Parameter:  %s\n", String_val(datetime_string));
+  printf("Zweiter Parameter: %s\n", String_val(format_string));
+
+  rest = strptime(String_val(datetime_string), String_val(format_string), &timestruct);
+  if( rest == NULL )
+  {
+    caml_failwith("Parsing error");
+  }
+  else if( strlen(rest) == 0 )
+  {
+    //fprintf(stderr, "Anything is fine!\n");
+    ;
+  }
+  else
+  {
+    //fprintf(stderr, "Not completely parsed (but no error)!\n");
+    ;
+  }
+
+  time = mktime(&timestruct);
+  //printf("Zeitwert: %ld\n", time);
 
 
-
-
-  //lept_retval = pixReadHeader(name, &pformat, &pw, &ph, &pbps, &pspp, &piscmap);
-
-  /* store results in Caml-value! :-) */
-  /* -------------------------------- */
-  //Store_field (result, 0, Double_val(time));
-  //Store_field (result, 1, Val_long(ph));
-  //Double_val(time);
-
-
-  CAMLreturn( result );
+  CAMLreturn( Val_long(time) );
 }
