287a26f9f0b1d74f3a9796e3f87d467129919515
Oliver Bandel <oliver@first.in-berlin.de>
2020-07-27 17:59:50 +0200

    reduced 4 spaces to 2 spaces in main.ml

--- main.ml
+++ main.ml
@@ -18,9 +18,9 @@ open Querytypes
 module LE = Logentry
 
 
-let formatting = ref Comma_sep (* Default-value     *)
-let outputfile = ref None      (* None means stdout *)
-let colsep = 3                 (* number of blanks to seperate the columns *)
+let formatting = ref Comma_sep (* Default-value   *)
+let outputfile = ref None    (* None means stdout *)
+let colsep = 3         (* number of blanks to seperate the columns *)
 
 
 
@@ -30,14 +30,14 @@ let colsep = 3                 (* number of blanks to seperate the columns *)
 let query() =
 
 (* Abfrage-Query parsen :) *)
-    begin
-      try
-        let lexbuf = Lexing.from_channel stdin in
-          let query_result = Querygrammar.main Querylex.token lexbuf in
-          query_result
-        with   End_of_file -> (*prerr_endline "EOF found!"; *) Quit
-             | Parsing.Parse_error -> prerr_endline "Parse Error!"; Error
-    end
+  begin
+    try
+    let lexbuf = Lexing.from_channel stdin in
+      let query_result = Querygrammar.main Querylex.token lexbuf in
+      query_result
+    with   End_of_file -> (*prerr_endline "EOF found!"; *) Quit
+       | Parsing.Parse_error -> prerr_endline "Parse Error!"; Error
+  end
 
 
 (* ------------------------------------------------------------------------------------ *)
@@ -48,9 +48,9 @@ let get_widths li =
  let maxvals = Stdlib.Array.make width 0 in
 
  let rec trav li idx = match li with
-       []     -> ()
-     | hd::tl -> maxvals.(idx) <- max maxvals.(idx) (String.length hd);
-                 trav tl (idx+1)
+     []   -> ()
+   | hd::tl -> maxvals.(idx) <- max maxvals.(idx) (String.length hd);
+         trav tl (idx+1)
  in
    List.iter ( fun entrylist -> trav entrylist 0 ) li;
    maxvals   (* return value is the maxvals-array *)
@@ -63,17 +63,17 @@ let string_to_latexstring str =
   let buf = Buffer.create 1000 in
   for idx = 0 to String.length str - 1
   do
-    match str.[idx] with
-      | '_' -> Buffer.add_string buf "\\_"
-      | '&' -> Buffer.add_string buf "\\&"
-      | '$' -> Buffer.add_string buf "\\$"
-      | '#' -> Buffer.add_string buf "\\#"
-      | '{' -> Buffer.add_string buf "\\{"
-      | '}' -> Buffer.add_string buf "\\}"
-      | '%' -> Buffer.add_string buf "\\%"
-      | '~' -> Buffer.add_string buf "\\textasciitilde"
+  match str.[idx] with
+    | '_' -> Buffer.add_string buf "\\_"
+    | '&' -> Buffer.add_string buf "\\&"
+    | '$' -> Buffer.add_string buf "\\$"
+    | '#' -> Buffer.add_string buf "\\#"
+    | '{' -> Buffer.add_string buf "\\{"
+    | '}' -> Buffer.add_string buf "\\}"
+    | '%' -> Buffer.add_string buf "\\%"
+    | '~' -> Buffer.add_string buf "\\textasciitilde"
 
-      | c   -> Buffer.add_char buf c
+    | c   -> Buffer.add_char buf c
   done;
   Buffer.contents buf
 
@@ -85,132 +85,132 @@ let string_to_latexstring str =
 (* ========== *)
 let rec loop() =
   print_string "# "; flush stdout; (* print a prompt for the user *)
-  match query()                    (* read user-input *)
+  match query()          (* read user-input *)
   with   Error -> prerr_endline "ERRRRRRROR!";
-                  prerr_endline "try it again! ";
-                  loop()
+          prerr_endline "try it again! ";
+          loop()
 
-       | Quit        -> prerr_endline "Beehren Sie uns auch beim naechsten mal wieder."; exit 0
-       | Output file -> outputfile := Some file;
-                        loop()
-                        (*
-                        begin
-                          match file with
-                            None -> ()
-                          | Some f -> 
-                                      print_endline ("Not implemented!  should print to file " ^ f)
-                        end
-                        *)
-       | Format form -> begin
-                          match form with
-                              Comma_sep -> formatting := Comma_sep; prerr_endline "format set to DEFAULT (comma-space seperated)"
-                            | Aligned   -> formatting := Aligned; prerr_endline "format set to ALIGNED (three whitespaces)"
-                            | LaTeX     -> formatting := LaTeX; prerr_endline "LaTeX-Table-format is not implemented so far, sorry!"
-                            | Html      -> formatting := Html; prerr_endline "format set to HTMl (html-table)"
+     | Quit    -> prerr_endline "Beehren Sie uns auch beim naechsten mal wieder."; exit 0
+     | Output file -> outputfile := Some file;
+            loop()
+            (*
+            begin
+              match file with
+              None -> ()
+              | Some f -> 
+                    print_endline ("Not implemented!  should print to file " ^ f)
+            end
+            *)
+     | Format form -> begin
+              match form with
+                Comma_sep -> formatting := Comma_sep; prerr_endline "format set to DEFAULT (comma-space seperated)"
+              | Aligned   -> formatting := Aligned; prerr_endline "format set to ALIGNED (three whitespaces)"
+              | LaTeX   -> formatting := LaTeX; prerr_endline "LaTeX-Table-format is not implemented so far, sorry!"
+              | Html    -> formatting := Html; prerr_endline "format set to HTMl (html-table)"
 
-                        end;
-                        loop()
+            end;
+            loop()
 
-       | Query query -> (* from the successful query we got this result *)
+     | Query query -> (* from the successful query we got this result *)
 
-               let filter = match query.cond with
-                   None      -> fun anything -> true
-                 | Some filt -> filt
-               in
+         let filter = match query.cond with
+           None    -> fun anything -> true
+         | Some filt -> filt
+         in
 
-               begin
-                 try
-                   (* Reading logfile with WHERE-filter; only filtered entries in result *)
-                   (* ------------------------------------------------------------------ *)
-                   let filtered_entrylist = Readcombinedlog.readlog query.file filter in
-
-
-                   (* here we sort the GROUP-BY stuff *)
-                   (* =============================== *)
-
-                   (* --------------------------------------------------------------------------------------------- *)
-                   (* substitute query-Types by the strings they stand for                                          *)
-                   (* --------------------------------------------------------------------------------------------- *)
-                   (* Here we also add the GROUPing-data: a pair will be constructed, which contains the            *)
-                   (* elements that will be used for the sort.   Because these elements possibly are not inside     *)
-                   (* the query itself, they must be added to have them as sort-keys inside the datastructure.      *)
-                   (* To use a pair here, seperates the query from the GROUP-/sorting-stuff.                        *)
-                   (* The only reason why this is added, is, to have the possibility of sorting.                    *)
-                   (* The sort will be possible, even if the sort-keys (e.g. "referrer") are not inside the query   *)
-                   (* itself.                                                                                       *)
-                   (* --------------------------------------------------------------------------------------------- *)
-                   let sorted_filtered_stringlist_pair =
-                      List.map (fun entry -> (LE.getval_by_typelist entry query.sel,
-                                              LE.getval_by_typelist entry query.sort) ) filtered_entrylist in
-
-                   (* SORT (GROUP BY) the result by the GROUP-BY-selection *)
-                   (* ---------------------------------------------------- *)
-                   (* sorting the list by the sort-items *)
-                   let sorted_stuff = List.sort (fun el1 el2 -> compare (snd el1) (snd el2)) sorted_filtered_stringlist_pair in
-
-                   (* now we peel of the data from the sorted list *)
-                   let sorted_data = List.map (fun (comp1, comp2) -> comp1 ) sorted_stuff in
+         begin
+         try
+           (* Reading logfile with WHERE-filter; only filtered entries in result *)
+           (* ------------------------------------------------------------------ *)
+           let filtered_entrylist = Readcombinedlog.readlog query.file filter in
 
 
+           (* here we sort the GROUP-BY stuff *)
+           (* =============================== *)
 
-                   (* print out the result *)
-                   (* ==================== *)
-                   (* only the first component will be printed, because it contains the data *)
-                   (* SELECT OUTCHANNEL! here!  *)
-                   let outchannel = begin match !outputfile with None   -> stdout | Some f -> open_out f end in
+           (* --------------------------------------------------------------------------------------------- *)
+           (* substitute query-Types by the strings they stand for                      *)
+           (* --------------------------------------------------------------------------------------------- *)
+           (* Here we also add the GROUPing-data: a pair will be constructed, which contains the      *)
+           (* elements that will be used for the sort.   Because these elements possibly are not inside   *)
+           (* the query itself, they must be added to have them as sort-keys inside the datastructure.    *)
+           (* To use a pair here, seperates the query from the GROUP-/sorting-stuff.            *)
+           (* The only reason why this is added, is, to have the possibility of sorting.          *)
+           (* The sort will be possible, even if the sort-keys (e.g. "referrer") are not inside the query   *)
+           (* itself.                                             *)
+           (* --------------------------------------------------------------------------------------------- *)
+           let sorted_filtered_stringlist_pair =
+            List.map (fun entry -> (LE.getval_by_typelist entry query.sel,
+                        LE.getval_by_typelist entry query.sort) ) filtered_entrylist in
 
-                   begin
-                     (* depending on the selected format for the output, we put out *)
-                     match !formatting with
-                         Comma_sep -> List.iter (fun p -> Printf.fprintf outchannel "%s\n" (String.concat ", " p ) ) sorted_data
-                       | Aligned   -> 
-                                      let maxlength_arr = get_widths sorted_data in
+           (* SORT (GROUP BY) the result by the GROUP-BY-selection *)
+           (* ---------------------------------------------------- *)
+           (* sorting the list by the sort-items *)
+           let sorted_stuff = List.sort (fun el1 el2 -> compare (snd el1) (snd el2)) sorted_filtered_stringlist_pair in
 
-                                      (* this function prints one entry-line *)
-                                      (* ----------------------------------- *)
-                                        let rec trav li idx lenarr =
-                                            match li with
-                                              []     -> ()
-                                            | hd::tl ->
-                                                        output_string outchannel hd; (* the text itself *)
-                                                        (* the needed blanks: colsep spaces more as column-seperator *)
-                                                        let spaces = String.make (lenarr.(idx) - String.length hd + colsep) ' '  in
-                                                        output_string outchannel spaces;
-                                                        trav tl (idx+1) lenarr (* next entry of the list *)
-                                        in
-                                          (* and here it will be applied *)
-                                          List.iter ( fun li -> trav li 0 maxlength_arr; output_char outchannel '\n' ) sorted_data
+           (* now we peel of the data from the sorted list *)
+           let sorted_data = List.map (fun (comp1, comp2) -> comp1 ) sorted_stuff in
 
-     (* =======>   ***************************************************** here: String-lengths! *)
 
-                       | LaTeX     -> output_string outchannel "should print with   LaTeX-Table - it is possibly wrong.\n";
 
-                                      (*"\begin{tabular}{ll}"*)
-                                      (* "|l|l|l|" *)
-                                      let n = List.length (List.hd sorted_data) in
-                                      let tabdef = (Bytes.make (2 * n + 1) '|' ) in
-                                      for idx = 1 to  n
-                                      do
-                                        Bytes.set tabdef (idx*2-1) 'l'
-                                      done;
-                                      output_string outchannel ( "\\begin{tabular}{" ^ (Bytes.to_string tabdef) ^ "}\\hline\n");
+           (* print out the result *)
+           (* ==================== *)
+           (* only the first component will be printed, because it contains the data *)
+           (* SELECT OUTCHANNEL! here!  *)
+           let outchannel = begin match !outputfile with None   -> stdout | Some f -> open_out f end in
 
-                                      List.iter (fun p -> let ltxstrlist = List.map string_to_latexstring p in (* LaTeX-Quotings! *)
-                                                          Printf.fprintf outchannel "%s\\\\\\hline\n" (String.concat "&" ltxstrlist)
-                                                ) sorted_data;
-                                      output_string outchannel "\\end{tabular}\n";
-                                      output_string outchannel "LaTeX-Table is possibly wrong, because of missing LateX-Quotations!\n"
+           begin
+           (* depending on the selected format for the output, we put out *)
+           match !formatting with
+             Comma_sep -> List.iter (fun p -> Printf.fprintf outchannel "%s\n" (String.concat ", " p ) ) sorted_data
+             | Aligned   -> 
+                    let maxlength_arr = get_widths sorted_data in
 
-                       | Html      -> output_string outchannel "<table border=\"1\">\n";
-                                      List.iter (fun p -> Printf.fprintf outchannel "  <tr>\n    <td>%s</td>\n  </tr>\n" (String.concat "</td><td>" p)) sorted_data;
-                                      output_string outchannel "</table>\n"
-                   end;
-                   flush outchannel;
-                   if !outputfile <> None then close_out outchannel
+                    (* this function prints one entry-line *)
+                    (* ----------------------------------- *)
+                    let rec trav li idx lenarr =
+                      match li with
+                        []   -> ()
+                      | hd::tl ->
+                            output_string outchannel hd; (* the text itself *)
+                            (* the needed blanks: colsep spaces more as column-seperator *)
+                            let spaces = String.make (lenarr.(idx) - String.length hd + colsep) ' '  in
+                            output_string outchannel spaces;
+                            trav tl (idx+1) lenarr (* next entry of the list *)
+                    in
+                      (* and here it will be applied *)
+                      List.iter ( fun li -> trav li 0 maxlength_arr; output_char outchannel '\n' ) sorted_data
 
-                 with Sys_error msg -> Printf.printf "Error: %s\n" msg (* possibly logfile does not exist *)
-               end;
-               loop()
+   (* =======>   ***************************************************** here: String-lengths! *)
+
+             | LaTeX   -> output_string outchannel "should print with   LaTeX-Table - it is possibly wrong.\n";
+
+                    (*"\begin{tabular}{ll}"*)
+                    (* "|l|l|l|" *)
+                    let n = List.length (List.hd sorted_data) in
+                    let tabdef = (Bytes.make (2 * n + 1) '|' ) in
+                    for idx = 1 to  n
+                    do
+                    Bytes.set tabdef (idx*2-1) 'l'
+                    done;
+                    output_string outchannel ( "\\begin{tabular}{" ^ (Bytes.to_string tabdef) ^ "}\\hline\n");
+
+                    List.iter (fun p -> let ltxstrlist = List.map string_to_latexstring p in (* LaTeX-Quotings! *)
+                              Printf.fprintf outchannel "%s\\\\\\hline\n" (String.concat "&" ltxstrlist)
+                        ) sorted_data;
+                    output_string outchannel "\\end{tabular}\n";
+                    output_string outchannel "LaTeX-Table is possibly wrong, because of missing LateX-Quotations!\n"
+
+             | Html    -> output_string outchannel "<table border=\"1\">\n";
+                    List.iter (fun p -> Printf.fprintf outchannel "  <tr>\n  <td>%s</td>\n  </tr>\n" (String.concat "</td><td>" p)) sorted_data;
+                    output_string outchannel "</table>\n"
+           end;
+           flush outchannel;
+           if !outputfile <> None then close_out outchannel
+
+         with Sys_error msg -> Printf.printf "Error: %s\n" msg (* possibly logfile does not exist *)
+         end;
+         loop()
 
 
 
